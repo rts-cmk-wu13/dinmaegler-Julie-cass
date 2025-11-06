9ac0fc27831916a6b4db3b667fab1961146d72e2
Julie Cassandra Gerner Bissø <jbissoe@gmail.com>
2025-11-06 12:18:28 +0100

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import Search from "../components/search/search.jsx";
 import Danes from "../components/danes/danes";
-import Employees from "../components/workers/emoployes.jsx";
+import Chosen from "../components/chosen/chosen.jsx";   
 import Newsletter from "../components/newsletter/Newsletter.jsx";   
+import Employees from "../components/workers/emoployes.jsx";
+
 function Home() {
     return(
         <>
 
 <Search />
 <Danes />
-
+<Chosen />
 <Newsletter />
 <Employees />
         </>
