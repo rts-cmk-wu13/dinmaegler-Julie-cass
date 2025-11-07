c5056e990cb70367753fc47c3e03c040add55cc3
Julie Cassandra Gerner Bissø <jbissoe@gmail.com>
2025-11-07 14:05:17 +0100

    fixed it for you

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Menu from "./components/headerNavi/menu.jsx";
+import Footer from "./components/footer/footer.jsx";
 
 function Layout() {
   return (
@@ -10,6 +11,8 @@ function Layout() {
       <main>
         <Outlet />
       </main>
+
+<Footer />
     </>
   );
 }
