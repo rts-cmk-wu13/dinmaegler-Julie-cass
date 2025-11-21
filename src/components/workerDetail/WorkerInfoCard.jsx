bd92bb4c435877185c1cd6f49af7b01d729a4b7a
Julie Cassandra Gerner Bissø <jbissoe@gmail.com>
2025-11-21 12:42:43 +0100

    made the contact page and added the heart to favor a worker

--- src/components/workerDetail/WorkerInfoCard.jsx
+++ src/components/workerDetail/WorkerInfoCard.jsx
@@ -3,18 +3,21 @@ import { useParams } from "react-router-dom";
 
 import { SiMinutemailer } from "react-icons/si";
 import { FaInstagram } from "react-icons/fa6";
-import { FaLinkedinIn, FaSkype, FaPhoneAlt  } from "react-icons/fa";
-/* import { FaSkype } from "react-icons/fa"; */
+import { FaLinkedinIn, FaSkype, FaPhoneAlt, FaRegHeart,  FaHeart  } from "react-icons/fa";
+
 
 import "./detailworker-scss/Carddetail.scss";
 import useWorker from "./useWorker.jsx";
 
 function WorkerInfoCard() {
+  const [liked, setLiked] = useState(false);
+
   const { id } = useParams();
   const { worker, loading } = useWorker(id);
 
   if (loading) return <p>Loading...</p>;
   if (!worker) return <p>No worker found.</p>;
+
 
 
   return (
@@ -32,10 +35,18 @@ function WorkerInfoCard() {
 
           </div>
           <div className="D_worker_text_info">
-            <p className="name">{worker.name}</p>
+            <div className="name_heart">
+              <p className="name">{worker.name}</p>
+              {liked ? (
+    <FaHeart className="heart-icon liked" onClick={() => setLiked(false)} />
+  ) : (
+    <FaRegHeart className="heart-icon" onClick={() => setLiked(true)} />
+  )}
+             
+            </div>
             <p className="title">{worker.title}</p>
-            <p className="phone"><FaPhoneAlt/>{worker.phone}</p>
-            <p className="email"><SiMinutemailer/>{worker.email}</p>
+            <p className="phone"><FaPhoneAlt />{worker.phone}</p>
+            <p className="email"><SiMinutemailer />{worker.email}</p>
           </div>
 
         </div>
