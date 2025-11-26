070767314fbe5f79477080c3df1be8f6bc477484
Sidsel Thomsen <U2469819@rts-365.dk>
2025-11-26 13:57:52 +0100

    Filter DONE

--- src/pages/PropList.jsx
+++ src/pages/PropList.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Buildings from "../components/buildings/buildings.jsx";
-import PagesBg from "../components/pagesTitles/pagesBg.jsx";  
+import PagesBg from "../components/pagesTitles/pagesBg.jsx"; 
+import Buildingsearch from "../components/search/buildingsearch.jsx"; 
+
 function List() {
-    return(
-        <>
-    <PagesBg />
-    <h2>search bar for buildings go here</h2>
-    <Buildings />
-        </>
-    )
+  const [filters, setFilters] = useState({ category: "", minPrice: 0, maxPrice: 12000000 });
+
+  return (
+    <>
+      <PagesBg />
+      <br />
+      <Buildingsearch onFiltersChange={setFilters} />
+      <Buildings filters={filters} />
+    </>
+  );
 }
+
 export default List;
